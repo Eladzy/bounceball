6885d91f5ba61093753b63e77fcc7adb59f94a2d
Eladzy <45952716+Eladzy@users.noreply.github.com>
2019-07-16 14:56:25 +0300

    Add files via upload

---
+++ ball/ball/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ballPic = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.ballPic.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.ballPic.Location = new System.Drawing.Point(393, 116);
+            this.ballPic.Name = "pictureBox1";
+            this.ballPic.Size = new System.Drawing.Size(201, 201);
+            this.ballPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ballPic.TabIndex = 0;
+            this.ballPic.TabStop = false;
+            this.ballPic.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(984, 514);
+            this.Controls.Add(this.ballPic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ballPic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ballPic;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ball/ball/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ball
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer timer=new System.Windows.Forms.Timer();
+        private enum Direction
+        {
+            positive,
+            negative,
+        }
+       
+        private Direction directionY = Direction.positive;
+        private Direction directionX = Direction.positive;
+        public Form1()
+        {
+            InitializeComponent();
+            this.timer.Enabled = false;
+            this.timer.Interval = 10;
+            this.timer.Tick += Move;//every 10 ms move event will be called
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            RandomizeLocation();
+            this.timer.Enabled = true;
+            
+        }
+        private void Move(object sender,EventArgs args)//will be called on tick
+        {
+            
+            int xUpperLimit = this.ClientSize.Width- ballPic.Width;//window width minus picturebox width
+            int LowerLimit = 0;//(0,0)
+            int yUpperLimit = this.ClientSize.Height-ballPic.Height;//window width minus picturebox height
+            Point current = ballPic.Location;//original location on tick
+            Task.Run(() => 
+            {
+                if (ballPic.Location.X >= xUpperLimit)
+                {
+                    directionX = Direction.negative;
+                }
+                if (ballPic.Location.X <= LowerLimit)
+                {
+                    directionX = Direction.positive;
+                }
+                if (directionX == Direction.positive)
+                {
+                    current.X++;//location transform on the positive diretion of x axis
+                }
+                else if(directionX==Direction.negative)//location transform on the negative diretion of x axis
+                {
+                    current.X--;
+                }
+                if (ballPic.Location.Y >= yUpperLimit)//location transform on the positive diretion of y axis
+                {
+                    directionY = Direction.positive;
+                }
+                if (ballPic.Location.Y <= LowerLimit)
+                {
+                    directionY = Direction.negative;
+                }
+                if (directionY == Direction.positive)//location transform on the negative diretion of y axis
+                {
+                    current.Y--;
+                }
+                else 
+                {
+                    current.Y++;
+                }
+                Action action = () => ballPic.Location = current;
+                ballPic.BeginInvoke(action);
+            });
+            
+        }
+        private void RandomizeLocation()
+        {
+            Random rnd = new Random();
+            Point position = ballPic.Location;
+            int maxHeight = (int)ClientSize.Height;
+            int maxWidth = (int)ClientSize.Width;
+            position.X = rnd.Next(maxWidth);
+            position.Y = rnd.Next(maxHeight);
+            ballPic.Location = position;
+        }
+    }
+}
